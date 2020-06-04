c4edc4e915f4bfe0cc32171953f2604ed520674f
Nirbhay <da201501068@gmail.com>
2020-06-04 22:37:35 +0530

    Some attributes added in User class.

--- src/main/java/com/nirbhayram/org/accessingdatamysql/MainController.java
+++ src/main/java/com/nirbhayram/org/accessingdatamysql/MainController.java
@@ -15,16 +15,9 @@ public class MainController {
   private UserRepository userRepository;
 
   @PostMapping(path="/add") // Map ONLY POST Requests
-  public @ResponseBody String addNewUser (@RequestParam String name
-      , @RequestParam String email) {
-    // @ResponseBody means the returned String is the response, not a view name
-    // @RequestParam means it is a parameter from the GET or POST request
-
-    User n = new User();
-    n.setName(name);
-    n.setEmail(email);
-    userRepository.save(n);
-    return "Saved";
+  public @ResponseBody String addNewUser (@RequestBody User user) {
+    userRepository.save(user);
+    return "{'message':'user created'}";
   }
 
   @GetMapping(path="/user/{id}")
--- src/main/java/com/nirbhayram/org/accessingdatamysql/User.java
+++ src/main/java/com/nirbhayram/org/accessingdatamysql/User.java
@@ -1,20 +1,58 @@
 package com.nirbhayram.org.accessingdatamysql;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
+
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column(name = "userid")
   private Integer id;
 
-  private String name;
+  @Column(name = "lastname")
+  private String lastName;
 
+  @Column(name = "firstname")
+  private String firstName;
+
+  @Column(name = "email")
   private String email;
+
+
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public User(Integer id, String lastName, String firstName, String email) {
+    this.id = id;
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.email = email;
+  }
+
+  public User(String lastName, String firstName, String email) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.email = email;
+  }
+
+  public User() {
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
   public Integer getId() {
     return id;
@@ -22,14 +60,6 @@ public class User {
 
   public void setId(Integer id) {
     this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   public String getEmail() {
