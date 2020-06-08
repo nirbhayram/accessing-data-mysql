07b7c7fd7fba7f0ef1fe2d3d8b54dff87841fccd
Nirbhay <da201501068@gmail.com>
2020-06-08 21:39:22 +0530

    PreviousCourseID updated.

--- src/main/java/com/nirbhayram/org/accessingdatamysql/entity/previouscourse/PreviousCourseID.java
+++ src/main/java/com/nirbhayram/org/accessingdatamysql/entity/previouscourse/PreviousCourseID.java
@@ -1,37 +1,45 @@
-//package com.nirbhayram.org.accessingdatamysql.entity.previouscourse;
-//
-//import com.nirbhayram.org.accessingdatamysql.entity.medicine.Medicine;
-//import com.nirbhayram.org.accessingdatamysql.entity.user.User;
-//
-//import java.io.Serializable;
-//import java.util.Objects;
-//
-//public class PreviousCourseID implements Serializable {
-//
-//    private User user;
-//
-//    private Medicine medicine;
-//
-//    public PreviousCourseID(User user, Medicine medicine) {
-//        this.user = user;
-//        this.medicine = medicine;
-//    }
-//
-//    public PreviousCourseID() {
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(user.getId(),medicine.getId());
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this==o)
-//            return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        PreviousCourseID previousCourseId = (PreviousCourseID) o;
-//        return previousCourseId.user.getId()==this.user.getId() &&
-//                previousCourseId.medicine.getId()==this.medicine.getId();
-//    }
-//}
+package com.nirbhayram.org.accessingdatamysql.entity.previouscourse;
+
+import com.nirbhayram.org.accessingdatamysql.entity.medicine.Medicine;
+import com.nirbhayram.org.accessingdatamysql.entity.user.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class PreviousCourseID implements Serializable {
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @ManyToOne
+    @JoinColumn
+    private Medicine medicine;
+
+    public PreviousCourseID() {
+    }
+
+    public PreviousCourseID(User user, Medicine medicine) {
+        this.user = user;
+        this.medicine = medicine;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user.getUserId(), medicine.getMedicineId());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PreviousCourseID previousCourseId = (PreviousCourseID) o;
+        return previousCourseId.user.getUserId() == this.user.getUserId() &&
+                previousCourseId.medicine.getMedicineId() == this.medicine.getMedicineId();
+    }
+}
