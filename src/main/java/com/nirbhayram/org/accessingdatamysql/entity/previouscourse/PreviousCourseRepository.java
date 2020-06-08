2e41b6df0f69a56c43c95a12f1e585bb2a9935ff
Nirbhay <da201501068@gmail.com>
2020-06-08 21:46:50 +0530

    PreviousCourseRepository updated.

--- src/main/java/com/nirbhayram/org/accessingdatamysql/entity/previouscourse/PreviousCourseRepository.java
+++ src/main/java/com/nirbhayram/org/accessingdatamysql/entity/previouscourse/PreviousCourseRepository.java
@@ -1,6 +1,11 @@
-//package com.nirbhayram.org.accessingdatamysql.entity.previouscourse;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface PreviousCourseRepository extends CrudRepository<PreviousCourse,PreviousCourseID> {
-//}
+package com.nirbhayram.org.accessingdatamysql.entity.previouscourse;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PreviousCourseRepository extends CrudRepository<PreviousCourse, PreviousCourseID> {
+    PreviousCourse findByPreviousCourseIDUserUserIdAndPreviousCourseIDMedicineMedicineId(int userId, int medicineId);
+
+    List<PreviousCourse> findByPreviousCourseIDUserUserId(int userId);
+}
