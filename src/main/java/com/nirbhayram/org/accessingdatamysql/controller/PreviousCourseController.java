e66b8759624207491b1b15da6655dfabdc5e4d17
Nirbhay <da201501068@gmail.com>
2020-06-09 13:57:40 +0530

    PreviousCourseController complete.

--- src/main/java/com/nirbhayram/org/accessingdatamysql/controller/PreviousCourseController.java
+++ src/main/java/com/nirbhayram/org/accessingdatamysql/controller/PreviousCourseController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/previous-course")
@@ -18,8 +20,8 @@ public class PreviousCourseController {
     private IPreviousCourseService previousCourseService;
 
     @GetMapping
-    public ResponseEntity<List<PreviousCourse>> getPreviousCourse() {
-        return null;
+    public ResponseEntity<List<PreviousCourse>> getPreviousCourse(@RequestParam Map<Object, Object> map) {
+        return previousCourseService.getPreviousCourse(map);
     }
 
 }
