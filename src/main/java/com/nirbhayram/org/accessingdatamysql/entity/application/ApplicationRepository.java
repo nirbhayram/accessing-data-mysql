d9984a7f761d89bd7b6f4b72d53accec151fd27c
Nirbhay <da201501068@gmail.com>
2020-06-09 11:47:35 +0530

    Removing Application

--- src/main/java/com/nirbhayram/org/accessingdatamysql/entity/application/ApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nirbhayram.org.accessingdatamysql.entity.application;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface ApplicationRepository extends CrudRepository<Application,Integer> {
-    List<Application> findByNameAndOwner(String name,String owner);
-}
--- src/main/java/com/nirbhayram/org/accessingdatamysql/service/application/IApplicationService.java
+++
@@ -1,7 +0,0 @@
-package com.nirbhayram.org.accessingdatamysql.service.application;
-
-import com.nirbhayram.org.accessingdatamysql.entity.application.Application;
-
-public interface IApplicationService {
-    boolean addApplication(Application application);
-}
